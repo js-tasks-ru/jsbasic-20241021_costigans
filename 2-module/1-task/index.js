1ed9ddd0cb1ccc6e75083f3903edb31ca49d6cf1
Konstantin Sokolov <konstantin.sokolov@me.com>
2024-10-31 12:12:19 +0300

    Реализация первой задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  
+  for (let key in salaries) {
+    if (
+      typeof salaries[key] === 'number' &&
+      isFinite(salaries[key]) &&
+      !isNaN(salaries[key])
+    ) {
+      sum += salaries[key];
+    }
+  }
+
+  return sum;
 }
