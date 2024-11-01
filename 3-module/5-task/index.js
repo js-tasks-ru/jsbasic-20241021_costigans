e029b550fcd9855d167787a8dda3e65005118cef
Konstantin Sokolov <konstantin.sokolov@me.com>
2024-11-01 15:49:06 +0300

    Реализация пятой задачи

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str
+    .split(' ')
+    .filter(value => isFinite(value))
+    .map(value => Number(value))
+    .sort((a, b) => a - b);
+
+  return {
+    min: arr[0],
+    max: arr.at(-1),
+  }
 }
