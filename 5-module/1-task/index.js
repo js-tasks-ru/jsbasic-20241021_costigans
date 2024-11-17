610575bb8d76cde67673fe8f10ddd3303e469722
Konstantin Sokolov <konstantin.sokolov@me.com>
2024-11-17 22:23:57 +0300

    Рефакторинг первой задачи

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,4 +1,4 @@
 function hideSelf() {
   const button = document.querySelector('.hide-self-button');
-  button.addEventListener('click', ({target}) => target.hidden = true);
+  button.addEventListener('click', ({target}) => target.hidden = true, {once: true});
 }
