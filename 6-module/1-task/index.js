1f3d1f0d6afc2751fb79e7e01a232960c671f380
Konstantin Sokolov <konstantin.sokolov@me.com>
2024-11-26 13:50:56 +0300

    Реализация задачи 6.1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,59 @@
  *      }
  *
  */
+import createElement from '../../assets/lib/create-element.js';
+
 export default class UserTable {
+  elem = null;
+
+  #rows = [];
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+
+    this.#render();
+  }
+
+  #template() {
+    return `
+      <table>
+          <thead>
+              <tr>
+                  <th>Имя</th>
+                  <th>Возраст</th>
+                  <th>Зарплата</th>
+                  <th>Город</th>
+                  <th></th>
+              </tr>
+          </thead>
+          <tbody>
+              ${this.#rows
+                .map(
+                  (row) => `      
+                  <tr>
+                      <td>${row.name}</td>
+                      <td>${row.age}</td>
+                      <td>${row.salary}</td>
+                      <td>${row.city}</td>
+                      <td><button>X</button></td>
+                  </tr>
+                `
+                )
+                .join("\n")}
+          </tbody>
+      </table>
+      `;
+  }
+
+  #onButtonClick = ({ target }) => {
+    if (target.tagName !== "BUTTON") {
+      return;
+    }
+    target.closest("tr").remove();
+  };
+
+  #render() {
+    this.elem = createElement(this.#template());
+    this.elem.addEventListener("click", this.#onButtonClick);
   }
 }
